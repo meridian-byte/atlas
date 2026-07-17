52c2f91cb73e60d756890d8f540aa117a2339d7b
kevon <dev@kevon.net>
2026-07-17 12:04:58 +0300

    feat: add event, link, note, and workspace action handlers; update imports in active item store

--- packages/handlers/src/index.ts
+++ packages/handlers/src/index.ts
@@ -5,8 +5,3 @@ export * from './requests/database/events';
 export * from './requests/database/links';
 export * from './requests/database/notes';
 export * from './requests/database/profiles';
-
-export * from './state/event';
-export * from './state/link';
-export * from './state/note';
-export * from './state/workspace';
--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -5,6 +5,11 @@ export * from './state/initialize/session';
 export * from './state/initialize/user-state';
 export * from './state/initialize/view';
 
+export * from './state/actions/event';
+export * from './state/actions/link';
+export * from './state/actions/note';
+export * from './state/actions/workspace';
+
 export * from './state/active-items';
 export * from './state/appshell';
 export * from './state/event';
