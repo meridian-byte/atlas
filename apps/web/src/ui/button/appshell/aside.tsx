43f01c6a2ae26216bb835871a79d8acad0fbd984
kevon <dev@kevon.net>
2026-07-23 19:55:22 +0300

    feat: create custom hook for appshell child management and refactor Aside and Navbar components to utilize it

--- apps/web/src/ui/button/appshell/aside.tsx
+++ apps/web/src/ui/button/appshell/aside.tsx
@@ -1,45 +1,24 @@
 'use client';
 
 import React from 'react';
-import {
-  IconArrowBarLeft,
-  IconArrowBarRight,
-  IconLayoutSidebarLeftCollapse,
-  IconLayoutSidebarLeftExpand,
-  IconLayoutSidebarRightCollapse,
-  IconLayoutSidebarRightExpand,
-} from '@tabler/icons-react';
-import { ActionIcon, Group, Tooltip } from '@mantine/core';
-import { ICON_SIZE, ICON_STROKE_WIDTH, ICON_WRAPPER_SIZE } from '@repo/constants';
-import { useStoreAppShell, useStoreView } from '@repo/store';
+import { IconArrowBarLeft, IconArrowBarRight } from '@tabler/icons-react';
+import { ActionIcon } from '@mantine/core';
+import { ICON_STROKE_WIDTH } from '@repo/constants';
 import { SHELL_VALUES } from '@web/constants';
+import { useAppshellChild } from '@web/hooks/appshell';
 
 export default function Aside({
   options,
 }: {
   options?: { hideWhenOpen?: boolean; hideWhenClosed?: boolean };
 }) {
-  const asideChild = useStoreAppShell((s) => s.appshell?.child?.aside);
-  const toggleAsideChild = useStoreAppShell((s) => s.toggleAsideChild);
-
-  // Get your view setter
-  const setAsideViewValue = useStoreView((s) => s.setAsideViewValue);
+  const { asideChild, handleToggleChildAside } = useAppshellChild();
 
   const states = {
     iconRight: !asideChild ? IconArrowBarLeft : IconArrowBarRight,
   };
 
   const label = `${asideChild ? 'Collapse' : 'Expand'} Aside`;
-
-  const handleToggle = () => {
-    // If the aside is currently open and we are about to close it,
-    // scrub the asideView value so it doesn't try to reopen on refresh
-    if (asideChild) {
-      setAsideViewValue(null);
-    }
-
-    toggleAsideChild();
-  };
 
   return (
     <ActionIcon
@@ -48,7 +27,7 @@ export default function Aside({
       color="dark"
       aria-label={label}
       size={SHELL_VALUES.FOOTER.HEIGHT - 1}
-      onClick={handleToggle} // Use the new handler wrapper
+      onClick={handleToggleChildAside}
       display={!options?.hideWhenClosed ? undefined : asideChild ? undefined : 'none'}
       miw={47.43}
     >
